dccecbee1269a435457b2d601c8a3843e21678f4
jasonmao87 <jasonmao87@gmail.com>
2017-07-12 08:29:48 +0800

    chage file name !

--- rpcServer/src/main/java/com/bugull/farm/web/init/BaseDemoService.java
+++ rpcServer/src/main/java/com/bugull/farm/web/init/BaseDemoService.java
@@ -1,13 +1,13 @@
 package com.bugull.farm.web.init;
 
-import com.bugull.farm.web.helloApi;
+import com.bugull.farm.web.HelloDemoApi;
 import com.weibo.api.motan.config.springsupport.annotation.MotanService;
 
 /**
  * Created by JasonMao on 2017/7/11.
  */
 @MotanService(export = "8002")
-public class baseService implements helloApi {
+public class BaseDemoService implements HelloDemoApi {
 
     public String hellow() {
         System.out.println("草 ！");
